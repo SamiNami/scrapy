33f7d477e6ddc705bd3e578179fa63ebd9311c52
sami peralahti <sami.peralahti@vainu.io>
2018-08-20 18:46:56 -0700

    more advanced spider

--- quotes_spider/quotes_spider/spiders/quotes.py
+++ quotes_spider/quotes_spider/spiders/quotes.py
@@ -8,7 +8,12 @@ class QuotesSpider(scrapy.Spider):
     start_urls = ['http://quotes.toscrape.com/']
 
     def parse(self, response):
-        h1_tag = response.xpath('//h1/a/text()').extract_first()
-        tags = response.xpath('//*[@class="tag-item"]/a/text()').extract()
-
-        yield {"H1 Tag": h1_tag, "tags":tags}
+        # h1_tag = response.xpath('//h1/a/text()').extract_first()
+        # tags = response.xpath('//*[@class="tag-item"]/a/text()').extract()
+        #
+        # yield {"H1 Tag": h1_tag, "tags":tags}
+        quotes = response.xpath('//*[@class="quote"]')
+        for quote in quotes:
+            author = quote.xpath('.//*[@itemprop="author"]/text()').extract_first()
+            text = quote.xpath('.//*[@class="text"]/text()').extract_first()
+            tags = quote.xpath('.//*[@itemprop="keywords"]/@content').extract_first()
