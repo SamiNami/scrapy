06aee2bc2f91876c16dcf905bcb45efc41a90013
sami peralahti <sami.peralahti@vainu.io>
2018-08-23 17:33:54 -0700

    scraping with selenium

--- books_crawler/books_crawler/spiders/books.py
+++ books_crawler/books_crawler/spiders/books.py
@@ -1,17 +1,45 @@
 # -*- coding: utf-8 -*-
-from scrapy.spiders import CrawlSpider, Rule
+from scrapy.spiders import Spider
 from scrapy.linkextractors import LinkExtractor
+from selenium import webdriver
+from scrapy.selector import Selector
+from scrapy.http import Request
 
 
-class BooksSpider(CrawlSpider):
+class BooksSpider(Spider):
     name = 'books'
     allowed_domains = ['books.toscrape.com']
-    start_urls = ['http://books.toscrape.com/']
 
-    # lin extactor arguments:
-    #  deny_domains=('google.com') does not scrape these pages
-    #  allow=('music') only scrapes these pages
-    rules = (Rule(LinkExtractor(allow=('music')), callback='parse_page', follow=False),)
+    def start_requests(self):
+        self.driver = webdriver.Chrome('/Users/samiperalahti/withinsights/chrome-extension/node_modules/chromedriver/lib/chromedriver/chromedriver')
+        self.driver.get('http://books.toscrape.com')
 
-    def parse_page(self, response):
-        yield { 'URL': response.url }
+        sel = Selector(text=self.driver.page_source)
+        books = sel.xpath('//h3/a/@href').extract()
+
+        for book in books:
+            url = 'http://books.toscrape.com/' + book
+            yield Request(url, callback=self.parse_book)
+
+    def parse_book(self, response):
+        pass
+
+
+
+
+# from scrapy.spiders import CrawlSpider, Rule
+# from scrapy.linkextractors import LinkExtractor
+#
+#
+# class BooksSpider(CrawlSpider):
+#     name = 'books'
+#     allowed_domains = ['books.toscrape.com']
+#     start_urls = ['http://books.toscrape.com/']
+#
+#     # lin extactor arguments:
+#     #  deny_domains=('google.com') does not scrape these pages
+#     #  allow=('music') only scrapes these pages
+#     rules = (Rule(LinkExtractor(allow=('music')), callback='parse_page', follow=False),)
+#
+#     def parse_page(self, response):
+#         yield { 'URL': response.url }
