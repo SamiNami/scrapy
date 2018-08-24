68a4cdd4b81849a65b56c11bdf3491f6c6e6c912
sami peralahti <sami.peralahti@vainu.io>
2018-08-23 17:55:19 -0700

    iterating over pages with a while loop

--- books_crawler/books_crawler/spiders/books.py
+++ books_crawler/books_crawler/spiders/books.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
+from time import sleep
 from scrapy.spiders import Spider
 from scrapy.linkextractors import LinkExtractor
 from selenium import webdriver
 from scrapy.selector import Selector
 from scrapy.http import Request
+from selenium.common.exceptions import NoSuchElementException
 
 
 class BooksSpider(Spider):
@@ -20,6 +22,26 @@ class BooksSpider(Spider):
         for book in books:
             url = 'http://books.toscrape.com/' + book
             yield Request(url, callback=self.parse_book)
+
+        while True:
+            try:
+                next_page = self.driver.find_element_by_xpath('//a[text()="next"]')
+                sleep(3)
+                self.logger.info("Sleeping for 3 seconds")
+                next_page.click()
+
+                sel = Selector(text=self.driver.page_source)
+                books = sel.xpath('//h3/a/@href').extract()
+
+                for book in books:
+                    url = 'http://books.toscrape.com/catalogue/' + book
+                    yield Request(url, callback=self.parse_book)
+
+            except NoSuchElementException:
+                self.logger.info("No more pages to load.")
+                self.driver.quit()
+                break
+
 
     def parse_book(self, response):
         pass
